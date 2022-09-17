f6b7f6b3dc8d25ff640f2ae7391a9603f8e7b2d5
Ivan Tyukavkin <ivan.ru614@gmail.com>
2022-09-17 21:51:18 +0700

    Добавил структуру "параллелограм", функции инициализации, ввода, вывода и расчета площади

--- lab2.cpp
+++ lab2.cpp
@@ -16,22 +16,23 @@ typedef struct {
     TPoint coord;
 } Vector;
 
-void ReadVector(Vector* v) {
+void VectorInit(Vector* v, int x, int y) {
+    v->coord.X = x;
+    v->coord.Y = y;
+}
+
+void VectorRead(Vector* v) {
     printf("Введите координаты вектора (x, y): ");
     scanf("%d%d", &v->coord.X, &v->coord.Y);
     
 }
 
-void DisplayVector(Vector v) {
+void VectorDisplay(Vector v) {
     printf("Вектор с координатами: (%d,%d)\n", v.coord.X, v.coord.Y);
 }
 
-void InitVector(Vector* v, int x, int y) {
-    v->coord.X = x;
-    v->coord.Y = y;
-}
 
-double CalcVectLen(Vector v) {
+double VectCalcLen(Vector v) {
     double len;
     len = sqrt(v.coord.X * v.coord.X + v.coord.Y * v.coord.Y);
     return len;
@@ -48,24 +49,24 @@ typedef struct {
     int R;
 } Circle;
 
-void InitCircle(Circle* c, int x, int y, int r) {
+void CircleInit(Circle* c, int x, int y, int r) {
     c->center.X = x;
     c->center.Y = y;
     c->R = r;
 }
 
-void ReadCircle(Circle* c) {
+void CircleRead(Circle* c) {
     printf("Введите координаты центра окружности (x, y): ");
     scanf("%d%d", &c->center.X, &c->center.Y);
     printf("Введите радиус окружности (R>0): ");
     scanf("%d", &c->R);
 }
 
-void DisplayCircle(Circle c) {
+void CircleDisplay(Circle c) {
     printf("Окружность c центром: О(%d,%d) и радиусом R=%d\n", c.center.X, c.center.Y, c.R);
 }
 
-void PrintCircleEquation(Circle c) {
+void CirclePrintEquation(Circle c) {
     puts("Уравнение окружности:");
     if (c.center.X > 0) {
         if (c.center.Y > 0)
@@ -93,7 +94,7 @@ void PrintCircleEquation(Circle c) {
     }
 }
 
-double CalcCircleSquare(Circle c) {
+double CircleCalcSquare(Circle c) {
     double S;
     S = PI * c.R * c.R;
     return S;
@@ -103,7 +104,7 @@ typedef struct {
     TPoint A, B, C;
 } Triangle;
 
-void InitTriangle(Triangle* t, int x1, int y1, int x2, int y2, int x3, int y3 ) {
+void TriangleInit(Triangle* t, int x1, int y1, int x2, int y2, int x3, int y3 ) {
     t->A.X = x1;
     t->A.Y = y1;
     t->B.X = x2;
@@ -112,7 +113,7 @@ void InitTriangle(Triangle* t, int x1, int y1, int x2, int y2, int x3, int y3 ) 
     t->C.Y = y3;
 }
 
-void ReadTriangle(Triangle* t) {
+void TriangleRead(Triangle* t) {
     printf("Введите координаты точки А треугольника (x1, y1): ");
     scanf("%d%d", &t->A.X, &t->A.Y);
     printf("Введите координаты точки B треугольника (x2, y2): ");
@@ -121,19 +122,19 @@ void ReadTriangle(Triangle* t) {
     scanf("%d%d", &t->C.X, &t->C.Y);
 }
 
-void DisplayTriangle(Triangle t) {
+void TriangleDisplay(Triangle t) {
     printf("Треугольник с вершинами А(%d,%d), B(%d,%d), C(%d,%d)\n", t.A.X, t.A.Y, t.B.X, t.B.Y, t.C.X, t.C.Y);
 
 }
 
-double CalcTriangleSquare(Triangle t) {
+double TriangleCalcSquare(Triangle t) {
     int x1 = t.A.X, y1 = t.A.Y, x2 = t.B.X, y2 = t.B.Y, x3 = t.C.X, y3 = t.C.Y;
     double S;
     S = 0.5 * fabs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
     return S;
 }
 
-double CalcTrianglePerimeter(Triangle t) {
+double TriangleCalcPerimeter(Triangle t) {
     int x1 = t.A.X, y1 = t.A.Y, x2 = t.B.X, y2 = t.B.Y, x3 = t.C.X, y3 = t.C.Y;
     double AB = sqrt((x2 - x1) ^ (x2 - x1) + (y2 - y1) ^ (y2 - y1));
     double AC = sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
@@ -142,11 +143,45 @@ double CalcTrianglePerimeter(Triangle t) {
     return P;
 }
 
+typedef struct {
+    Vector a, b;
+} Parallelogram;
+
+void ParallelogramInit(Parallelogram* pr, int x1, int y1, int x2, int y2) {
+    VectorInit(&pr->a, x1, y1);
+    VectorInit(&pr->b, x2, y2);
+}
+
+void ReadParallelogram(Parallelogram* pr) {
+    int x1, y1, x2, y2;
+    printf("Введите координаты первого из векторов, на которых построен параллелограм (x1, y1): ");
+    scanf("%d%d", &x1, &y1);
+    printf("Введите координаты второго из векторов, на которых построен параллелограм (x1, y1): ");
+    scanf("%d%d", &x2, &y2);
+    ParallelogramInit(pr, x1, y1, x2, y2);
+}
+
+void ParallelogramDisplay(Parallelogram pr) {
+    puts("Параллелограм, построенный на векторах a и b:");
+    VectorDisplay(pr.a);
+    VectorDisplay(pr.b);
+}
+
+double ParallelogramCalcSquare(Parallelogram pr) {
+    double S, len_a, len_b, prod, cosinus, sinus;
+    len_a = VectCalcLen(pr.a);
+    len_b = VectCalcLen(pr.b);
+    prod = CalcScalarProd(pr.a, pr.b);
+    cosinus = prod / (len_a * len_b);
+    sinus = sqrt(1 - cosinus * cosinus);
+    S = len_a * len_b * sinus;
+    return S;
+}
+
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251); 
-
   
 }
 
